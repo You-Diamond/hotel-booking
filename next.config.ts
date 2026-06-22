78c07bda420397c59effdc8f80bd2cb045266f82
You-Diamond <g.dm1@mail.ru>
2026-06-23 00:50:16 +0300

    next.config.ts_Change

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true, // Локально Next.js не будет качать картинки через себя
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'salerussiatravel-marriottkrasnodar.ru',
+        hostname: '**',
         pathname: '/**',
       },
     ],
